63d6a42a5e8a8e36e36cfa65d0432805083939ef
Pader Xiong <paderx26@gmail.com>
2022-12-10 19:52:18 -0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+
+import { Routes, Route } from 'react-router-dom'
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesDirectoryPage   from './pages/CampsitesDirectoryPage';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
+
 function App() {
     return (
         <div className='App'>
               <Header/>
-              <CampsitesDirectoryPage />
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='contact' element={<ContactPage />} />
+                    <Route path='directory' element={<CampsitesDirectoryPage />} />
+                </Routes>
               <Footer/>
         </div>
     );
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,13 +4,13 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-};
-
-// export const selectCampsiteById = (id) => {
-//     return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // };
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
 
 // export const selectFeaturedCampsite = () => {
 //     return CAMPSITES.find((campsite) => campsite.featured);
