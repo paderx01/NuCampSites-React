196a5894b69c8cedd4951add06986af083797793
Pader Xiong <paderx26@gmail.com>
2022-12-17 12:26:54 -0600

    comment validation

--- src/features/partners/Partner.js
+++ src/features/partners/Partner.js
@@ -1,17 +1,17 @@
-const Partner = (partner) => {
+  const Partner = ({ partner }) => {
     if (partner) {
-        const { image, name , description } = partner
-        return (
-            <>
-                <img src={partner.partner.image} alt={partner.partner.name} style={{ width: '150px' }} />
-                <div className='m-4'>
-                    <h5 className='fw-bold'>{partner.partner.name}</h5>
-                        {partner.partner.description}
-                </div>
-            </>
-        )
-    }
-    return null;
-};
-
-  export default Partner;
+      const { image, name, description } = partner
+  
+      return (
+        <>
+          <img src={image} alt={name} style={{ width: '150px' }} />
+          <div className='m-4'>
+            <h5 className="fw-bold">{name}</h5>
+            {description}
+          </div>
+        </>
+      )
+    } return null;
+  }
+  
+  export default Partner
