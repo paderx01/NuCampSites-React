1591b275a4ab3d4110282d2721766d903ddb0ee5
Pader Xiong <paderx26@gmail.com>
2022-12-31 11:41:46 -0600

    Async campsites

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-
-import { Routes, Route } from 'react-router-dom'
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-              <Header/>
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='contact' element={<ContactPage />} />
-                    <Route path='directory' element={<CampsitesDirectoryPage />} />
-                    <Route path='about' element={<AboutPage />} />
-                    <Route
-                        path='directory/:campsiteId'
-                        element={<CampsiteDetailPage />}
-                />
-                </Routes>
-              <Footer/>
-        </div>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/utils/validateUserLoginForm.js
+++ src/utils/validateUserLoginForm.js
@@ -1,20 +1,16 @@
 export const validateUserLoginForm = (values) => {
   const errors = {};
-
   if (!values.username) {
     errors.username = "Required";
-  }
-
-  if (!values.password) {
-    errors.password = "Required";
-  }
-
-  if (values.username.length < 6) {
+  } else if (values.username.length < 6) {
     errors.username = "Must be at least 6 characters.";
   } else if (values.username.length <= 15) {
     errors.username = "Must be 15 characters or less";
   }
-  if (values.password.length <= 8) {
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length <= 8) {
     errors.password = "Must be at least 8 characters.";
   }
 
